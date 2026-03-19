3892f1661b69d61cf3916a89b69becc681215383
Renan Homiak <renanhomiakg@gmail.com>
2026-03-18 22:04:26 -0300

    segundo commit - repository

---
+++ ex03 (1)/ex03/src/main/java/com/homiak/ex03/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.homiak.ex03.repository;
+
+import com.homiak.ex03.models.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+}
